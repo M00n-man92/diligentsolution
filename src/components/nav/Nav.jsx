4e82ffe9c6783924789d9246f3686c1e16fcb256
M00n-man92 <azraelnumb00@gmail.com>
2023-06-09 04:44:23 +0300

    minimum prce adjustment on the navigation bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React from "react"
 import "./nav.scss"
 export default function Nav() {
   return (
-    <div className='nav'>
+    <div className="nav">
       <div className="logo">
-       <img src="./assets/Artboard 1@4x.png" alt="" />
+        <img src="./assets/Artboard 1@4x.png" alt="" />
       </div>
       <div className="sec">
-      <div className="first">
-        <a href='#about'>
-         <b>About Us</b>
-         
-        </a>
+        <div className="first">
+          <a href="#about">
+            <b>About Us</b>
+          </a>
+        </div>
+        <div className="first">
+          <a href="#principles">
+            <b>Our Principles</b>
+          </a>
+        </div>
+
+        <div className="first">
+          <a href="#services">
+            <b>Our Services</b>
+          </a>
+        </div>
+        <div className="first">
+          <a href="#contact us">
+            <b>Contact Us</b>
+          </a>
+        </div>
+        <div className="first">
+          <a href="https://bereketera.github.io/minnav/min.html">
+            <b>Minimum Price</b>
+          </a>
+        </div>
       </div>
-      <div className="first">
-        <a href='#principles'>
-          <b>Our Principles</b>
-        </a>
-      </div>
-      
-      <div className="first">
-      <a href='#services'>
-         <b>Our Services</b>
-          
-        </a>
-      </div>
-      <div className="first">
-        <a href='#contact us' >
-          <b>
-          Contact Us
-          </b>
-          
-        </a>
-      </div>
-      </div>
-      
     </div>
   )
 }
