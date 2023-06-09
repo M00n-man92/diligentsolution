2a8d55865e09bd896271331817a7aa89ccb645fa
M00n-man92 <azraelnumb00@gmail.com>
2023-06-09 09:58:21 +0300

    made the changes on the contct

--- src/components/ninth/Nine.jsx
+++ src/components/ninth/Nine.jsx
@@ -1,37 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import Axios from "axios"
-import "./nine.scss";
+import "./nine.scss"
 
-import { Phone, Email, LocationOn, Telegram, WhatsApp } from '@mui/icons-material';
-import TextField from "@mui/material/TextField";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import CircularProgress from "@mui/material/CircularProgress";
-import Select from '@mui/material/Select';
-import { Button } from '@mui/material';
+import {
+  Phone,
+  Email,
+  LocationOn,
+  Telegram,
+  WhatsApp,
+} from "@mui/icons-material"
+import TextField from "@mui/material/TextField"
+import Box from "@mui/material/Box"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import CircularProgress from "@mui/material/CircularProgress"
+import Select from "@mui/material/Select"
+import { Button } from "@mui/material"
 
-import { useMutation } from "react-query";
+import { useMutation } from "react-query"
 export default function Nine() {
   /// first form
-  const [subject, setSubject] = useState('');
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [subject, setSubject] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
 
   //second form
-  const [companyName, setCompanyName] = useState("");
-  const [commodity, setCommodity] = useState("");
-  const [otherAge, setOtherAge] = useState("");
-  const [quality, setQuality] = useState(0);
-  const [volume, setVolume] = useState(0);
+  const [companyName, setCompanyName] = useState("")
+  const [commodity, setCommodity] = useState("")
+  const [otherAge, setOtherAge] = useState("")
+  const [quality, setQuality] = useState(0)
+  const [volume, setVolume] = useState(0)
   const [processing, setProcessing] = useState("")
   const [anotherUnit, setAnotherUnit] = useState("")
-  const [unit, setUnit] = useState("");
-  const [price, setPrice] = useState(0);
+  const [unit, setUnit] = useState("")
+  const [price, setPrice] = useState(0)
   const [destination, setDestination] = useState("")
-  const [reuirment, setRequirment] = useState("");
+  const [reuirment, setRequirment] = useState("")
   const [period, setPeriod] = useState("")
 
   // handeler variables
@@ -39,57 +45,59 @@ export default function Nine() {
   const [error, setErrors] = useState(false)
 
   //var to change the forms
-  const [change, setChange] = useState(false);
-  const [color, setColor] = useState("red");
+  const [change, setChange] = useState(false)
+  const [color, setColor] = useState("red")
   const send = async (user) => {
     let reply
     if (change) {
-      reply = await Axios.post("https://api-production-b6bc.up.railway.app/api/user/coffee", user);
-    }
-    else {
-      reply = await Axios.post("https://api-production-b6bc.up.railway.app/api/user/anothercoffee", user);
+      reply = await Axios.post(
+        "https://api-production-b6bc.up.railway.app/api/user/coffee",
+        user
+      )
+    } else {
+      reply = await Axios.post(
+        "https://api-production-b6bc.up.railway.app/api/user/anothercoffee",
+        user
+      )
     }
 
     // const reply = await Axios.post("http://localhost:5000/api/user/coffee", user);
 
-    return reply.data;
-  };
+    return reply.data
+  }
 
   const { mutate, isLoading, isError } = useMutation(send, {
     onSuccess: (data) => {
       setErrors(false)
-      console.log(data);
-      setKnow(data.msg);
-      setColor("green");
+      console.log(data)
+      setKnow(data.msg)
+      setColor("green")
     },
     onError: (e) => {
-      
-      setErrors(true);
-      setKnow(e.response.data.error.message);
-      console.log("there lis a big erroe");
+      setErrors(true)
+      setKnow(e.response.data.error.message)
+      console.log("there lis a big erroe")
     },
-  });
+  })
 
   // this is for the inquires form
 
   // this is or the contact form
   const handleChange = async (e) => {
-    e.preventDefault();
-    setErrors(false);
-    setKnow("");
-    setColor("");
+    e.preventDefault()
+    setErrors(false)
+    setKnow("")
+    setColor("")
     if (change) {
-      const user = { name, email, message, subject };
+      const user = { name, email, message, subject }
       if (name.length > 1 && email.length > 1 && message.length > 1) {
-        mutate(user);
+        mutate(user)
       } else {
-
-        setErrors(true);
+        setErrors(true)
         setColor("red")
-        setKnow("please make sure you've correctly filled the boxes'");
+        setKnow("please make sure you've correctly filled the boxes'")
       }
-    }
-    else if (!change) {
+    } else if (!change) {
       const user = {
         companyName,
         name,
@@ -103,26 +111,23 @@ export default function Nine() {
         anotherUnit,
         destination,
         reuirment,
-        period
-      };
+        period,
+      }
       if (name.length > 1 && email.length > 1) {
-        mutate(user);
+        mutate(user)
       } else {
-        setErrors(true);
+        setErrors(true)
         setColor("red")
-        setKnow("please make sure you've given us your name and email'");
+        setKnow("please make sure you've given us your name and email'")
       }
     }
-  };
+  }
   // console.log(isError, isLoading, isSuccess)
   return (
-    <div className='nine' id="contact us">
+    <div className="nine" id="contact us">
       <div className="first">
         <div className="wahid">
-          <h2>
-            Contact us
-          </h2>
-
+          <h2>Contact us</h2>
         </div>
         <div className="duos">
           <div className="estenen">
@@ -135,7 +140,7 @@ export default function Nine() {
             <div className="image">
               <Phone />
             </div>
-            <span>+251 07 10 081 993</span>
+            <span>+251 7 10 081 993</span>
           </div>
           <div className="newone">
             <div className="iconsline">
@@ -158,7 +163,6 @@ export default function Nine() {
                 <Telegram className="socialicons" />
               </a>
             </div>
-
           </div>
           <div className="estenen">
             <div className="image">
@@ -172,136 +176,70 @@ export default function Nine() {
             </div>
             <span>Bole, Addis Ababa, Ethiopia</span>
           </div>
-
-
         </div>
 
         <div className="gps">
           <img src="./assets/gps.PNG" alt="" />
         </div>
-
       </div>
-
-      {change ?
-
+      <form></form>
+      {change ? (
         <div className="second">
           <div className="postiton">
-
             <Button
-
               style={{ backgroundColor: "#4A2C2A" }}
               className="button"
-              onClick={() => setChange(!change)}>
-              Contact Us
+              onClick={() => setChange(!change)}
+            >
+              <a
+                href="https://bereketera.github.io/paralel-form/min.html"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Contact Us
+              </a>
             </Button>
-            <Button
-              className="button"
-              onClick={() => setChange(!change)}>
-              Submit Request
+            <Button className="button" onClick={() => setChange(!change)}>
+              <a
+                href="https://bereketera.github.io/paralel-form/min.html"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Submit Request
+              </a>
             </Button>
           </div>
-          <div className="one">
-
-            <TextField className='texting' label="Company Name"
-              onChange={(e) => setName(e.target.value)}
-              fullWidth
-              type="text"
-              required />
-          </div>
-          <div className="one">
-
-              <TextField className='texting' label="Name"
-                onChange={(e) => setName(e.target.value)}
-                fullWidth
-                type="text"
-                required />
-            </div>
-          <div className="one">
-
-            <TextField
-              className='texting' label="Mail"
-              onChange={(e) => setEmail(e.target.value)}
-              fullWidth
-              type="email"
-              required
-            />
-          </div>
-          <div className="one">
-
-              <TextField className='texting' label="Subject"
-                onChange={(e) => setSubject(e.target.value)}
-                fullWidth
-                type="text"
-                required />
-            </div>
-          {/* <div className="one">
-
-            <Box className="texting">
-              <FormControl fullWidth>
-                <InputLabel >Coffee Origin</InputLabel>
-                <Select
-
-                  value={age}
-                  label="Coffee Profile"
-                  onChange={(e) => setAge(e.target.value)}
-                  required
-                >
-                  <MenuItem value="Djimmah">Djimmah</MenuItem>
-                  <MenuItem value="Hrrar">Harrar</MenuItem>
-                  <MenuItem value="Limu">Limu</MenuItem>
-                  <MenuItem value="Teppi">Teppi</MenuItem>
-                  <MenuItem value="Gujji">Gujji</MenuItem>
-                  <MenuItem value="Lekmpti">Lekempti</MenuItem>
-                  <MenuItem value="Sidamo">Sidamo</MenuItem>
-                  <MenuItem value="Yirgacheffee">Yirgacheffee</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-          </div> */}
-          <div className="one">
-
-
-            <TextField
-              className='texting' label="Message"
-              onChange={(e) => setMessage(e.target.value)}
-              fullWidth
-              type="text"
-              multiline
-              rows={5}
-              required
-            />
-          </div>
-          <div className="two">
-            <Button
-              onClick={(e) => handleChange(e)}
-              type="submit"
-              disabled={isLoading}
-              startIcon={isLoading ? <CircularProgress color="inherit" size={25} /> : null}
-            > Send</Button>
-            {error ? <span style={{ color: color, marginLeft: 40 }}> {know}</span> : <span style={{ color: color, marginLeft: 40 }}>{know}</span>}
-          </div>
-
         </div>
-
-        :
-
+      ) : (
         <div className="second">
           <div className="postiton">
-
             <Button
               className="button"
               style={{ backgroundColor: "#4A2C2A" }}
-              onClick={() => setChange(!change)}>
-              Submit Request
+              onClick={() => setChange(!change)}
+            >
+              <a
+                href="https://bereketera.github.io/paralel-form/min.html"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Submit Request
+              </a>
             </Button>
-            <Button
-              className="button"
-              onClick={() => setChange(!change)}>
-              Contact Us
+            <Button className="button" onClick={() => setChange(!change)}>
+              <a
+                href="https://bereketera.github.io/paralel-form/min.html"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Contact Us
+              </a>
             </Button>
-
           </div>
-          <div className="one">
+        </div>
+      )}
+
+      {/* second form  */}
+    </div>
+  )
+}
+/*  <div className="one">
             <TextField className='texting' label="Company Name"
               onChange={(e) => setCompanyName(e.target.value)}
               fullWidth
@@ -330,12 +268,8 @@ export default function Nine() {
           </div>
           <div className="position">
             <div className="one">
-              {/* <TextField className='texting' label="Commodity"
-                onChange={(e) => setCommodity(e.target.value)}
-                fullWidth
-                type="text"
-                required /> */}
-              <Box className="texting">
+              
+                <Box className="texting">
                 <FormControl fullWidth>
                   <InputLabel >Commodity</InputLabel>
                   <Select
@@ -447,12 +381,7 @@ export default function Nine() {
                     </Select>
                   </FormControl>
                 </Box>
-                {/* <TextField className='texting' label="Unit"
-                  onChange={(e) => setUnit(e.target.value)}
-                  value={unit}
-                  fullWidth
-                  type="text"
-                  required /> */}
+               
               </div>
             </div>
             <div className="position">
@@ -528,16 +457,78 @@ export default function Nine() {
               startIcon={isLoading ? <CircularProgress color="inherit" size={25} /> : null}
             > Send</Button>
             {isError || error ? <span style={{ color: color, marginLeft: 40 }}> {know}</span> : <span style={{ color: "green" }}>{know}</span>}
+          </div> */
+{
+  /* <div className="one">
+            <TextField
+              className="texting"
+              label="Company Name"
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              type="text"
+              required
+            />
           </div>
-
-        </div>
-      }
-
-
-
-      {/* second form  */}
-
-
-    </div >
-  )
+          <div className="one">
+            <TextField
+              className="texting"
+              label="Name"
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              type="text"
+              required
+            />
+          </div>
+          <div className="one">
+            <TextField
+              className="texting"
+              label="Mail"
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              type="email"
+              required
+            />
+          </div>
+          <div className="one">
+            <TextField
+              className="texting"
+              label="Subject"
+              onChange={(e) => setSubject(e.target.value)}
+              fullWidth
+              type="text"
+              required
+            />
+          </div>
+          <div className="one">
+            <TextField
+              className="texting"
+              label="Message"
+              onChange={(e) => setMessage(e.target.value)}
+              fullWidth
+              type="text"
+              multiline
+              rows={5}
+              required
+            />
+          </div>
+          <div className="two">
+            <Button
+              onClick={(e) => handleChange(e)}
+              type="submit"
+              disabled={isLoading}
+              startIcon={
+                isLoading ? (
+                  <CircularProgress color="inherit" size={25} />
+                ) : null
+              }
+            >
+              {" "}
+              Send
+            </Button>
+            {error ? (
+              <span style={{ color: color, marginLeft: 40 }}> {know}</span>
+            ) : (
+              <span style={{ color: color, marginLeft: 40 }}>{know}</span>
+            )}
+          </div> */
 }
